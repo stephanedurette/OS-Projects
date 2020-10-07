47dfbdbd591e727bad4b878ca9f893200b99c57b
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2020-10-07 01:21:22 -0400

    bonus
    average ping displayed in popup

--- Lab 3/main.c
+++ Lab 3/main.c
@@ -4,13 +4,46 @@
 
 GtkApplication *app;
 
+void GetAverageFromFile(char* out){
+
+  //Get the last line of the file
+  char tmp[1024];
+  FILE* fp = fopen("ping_res.txt", "r");
+  while(!feof(fp)) fgets(tmp, 1024, fp);
+  fclose(fp);
+  
+  //Get the substring containing the average
+  char* p = strtok (tmp,"//");
+  int i = 0;
+  while (p != NULL && i < 4){
+    p = strtok (NULL, "/");
+    i++;
+  }
+  
+  //store it in the buffer
+  strcpy(out, p);
+}
+
 void showWindow(){
   GtkWidget *window;
+  GtkWidget *ipLabel;
+  
   window = gtk_application_window_new (app);
   gtk_window_set_title (GTK_WINDOW (window), "Average Ping Response");
-  gtk_window_set_default_size (GTK_WINDOW (window), 250, 50);
+  gtk_window_set_default_size (GTK_WINDOW (window), 400, 150);
   gtk_widget_show_all(window);
   
+  char buf[256];
+  GetAverageFromFile(buf);
+  
+  ipLabel = gtk_label_new(buf);
+  gtk_label_set_use_markup (GTK_LABEL (ipLabel), TRUE);
+  g_object_set (ipLabel, "margin", 20, NULL);
+  gtk_container_add (GTK_CONTAINER (window), ipLabel);
+  gtk_label_set_line_wrap (GTK_LABEL (ipLabel), TRUE);
+  gtk_label_set_max_width_chars (GTK_LABEL (ipLabel), 30);
+  
+  gtk_widget_show_all(window);
 }
 
 void ping(GtkWidget *pingButton, GtkWidget *ipNumbers[]){
@@ -72,7 +105,7 @@ static void activate(GtkApplication* app, gpointer user_data)
 
 	number4 = gtk_entry_new();
 	gtk_grid_attach(GTK_GRID(grid), number4, 3,3,1,1);
-
+	
 	ipNumbers[0] = number1;
 	ipNumbers[1] = number2;
 	ipNumbers[2] = number3;
@@ -85,7 +118,13 @@ static void activate(GtkApplication* app, gpointer user_data)
 	gtk_grid_attach(GTK_GRID(grid), pingCount, 0,5,1,1);
 
 	ipNumbers[4] = pingCount;
-
+	
+	gtk_entry_set_text((GtkEntry*)number1, "192");
+	gtk_entry_set_text((GtkEntry*)number2, "168");
+	gtk_entry_set_text((GtkEntry*)number3, "0");
+	gtk_entry_set_text((GtkEntry*)number4, "1");
+	gtk_entry_set_text((GtkEntry*)pingCount, "5");
+	
 	//Create the pingButton
 	pingButton = gtk_button_new_with_label("Ping!");
 
