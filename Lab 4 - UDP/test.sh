9fae233074b03eeab83594109c7eff65129b615b
stephanedurette <35239397+stephanedurette@users.noreply.github.com>
2020-11-11 13:51:44 -0500

    f

--- Lab 4 - UDP/test.sh
+++ Lab 4 - UDP/test.sh
@@ -1,5 +1,8 @@
 gcc udpclient.c -o udpclient
 gcc udpserver.c -o udpserver
 
+gcc server.c -o server
+gcc client.c -o client
+
 #gnome-terminal --tab -e "/bin/bash -c './udpclient; exec /bin/bash -i'"
 #gnome-terminal --tab -e "/bin/bash -c './udpserver 3490; exec /bin/bash -i'"
